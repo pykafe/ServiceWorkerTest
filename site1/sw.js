73d658cdef574b697f7bbdd529ce2ba12d0dcd34
PeteCoward <peter@catalpa.io>
2016-08-23 10:33:32 +0900

    Add html to cache in install event

--- site1/sw.js
+++ site1/sw.js
@@ -1,4 +1,16 @@
+var CACHE_NAME = 'ourcache';
+
+var urlsToCache = ['ServiceWorkerTest/site1/']
+
 self.addEventListener('install', function(event) {
-    console.log('Service worker is installing')
+  console.log('Service worker is installing')
   // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  console.log('Hello wolrd');
 });
