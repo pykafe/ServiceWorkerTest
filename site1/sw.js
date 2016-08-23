1b646a186d71fa608a27c16cce9b3d67cc272f9d
PeteCoward <peter@catalpa.io>
2016-08-23 10:47:36 +0900

    add request cach response

--- site1/sw.js
+++ site1/sw.js
@@ -14,3 +14,17 @@ self.addEventListener('install', function(event) {
   );
   console.log('Hello wolrd');
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
